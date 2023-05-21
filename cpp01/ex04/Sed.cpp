b20cd764fe0be5a0616b2c2d52b211f95c6e9d75
marcocslima <marcocslima@gmail.com>
2023-05-21 12:20:22 +0000

    working in cpp01 ex04

--- cpp01/ex04/Sed.cpp
+++ cpp01/ex04/Sed.cpp
@@ -6,7 +6,7 @@
 /*   By: mcesar-d <mcesar-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 11:46:34 by mcesar-d          #+#    #+#             */
-/*   Updated: 2023/05/20 13:25:30 by mcesar-d         ###   ########.fr       */
+/*   Updated: 2023/05/21 12:19:14 by mcesar-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,28 @@ Sed::~Sed(void) {}
 
 void	Sed::replaceStr(std::string s1, std::string s2)
 {
-	std::string line;
-	
-	std::ifstream infile(this->_fileIn);
-	std::fstream outfile(this->_fileOut, std::ios::in | std::ios::out);
-	if (!infile)
+	std::string content;
+	std::ifstream infile;
+	std::ofstream outfile;
+
+	infile.open(this->_fileIn.c_str());
+	outfile.open(this->_fileOut.c_str());
+	if (!infile.is_open() && !outfile.is_open())
 	{
 		std::cerr << "Error opening files" << std::endl;
 		return ;
 	}
-	while (std::getline(infile, line))
-		outfile << line << std::endl;
-	infile.close();
-	while (std::getline(outfile, line))
+	while (std::getline(infile, content, '\0'))
 	{
-		size_t pos = line.find(s1);
+		size_t pos = content.find(s1);
 		while (pos!= std::string::npos)
 		{
-			outfile << line.substr(0, pos);
-			outfile << s2;
-			pos = line.find(s1, pos + s1.length());
+			content.erase(pos, s1.length());
+			content.insert(pos, s2);
+			pos = content.find(s1);
 		}
-		outfile << line.substr(pos);
-		outfile << std::endl;
+		outfile << content;
+		outfile.close();
 	}
-	outfile.close();
+	infile.close();
 }
