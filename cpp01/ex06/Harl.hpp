ac148125cfb49ce88aa16d1109c318baaeb67ec8
marcocslima <marcocslima@gmail.com>
2023-05-21 20:12:41 +0000

    cpp01 finished

--- cpp01/ex06/Harl.hpp
+++ cpp01/ex06/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: mcesar-d <mcesar-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 18:22:40 by mcesar-d          #+#    #+#             */
-/*   Updated: 2023/05/21 18:51:58 by mcesar-d         ###   ########.fr       */
+/*   Updated: 2023/05/21 20:11:11 by mcesar-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,5 @@ class Harl
 		~Harl();
 		void	complain(std::string level);
 };
-
-typedef	void (Harl::*ptr_func)(void);
 
 #endif
