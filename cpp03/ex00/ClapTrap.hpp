f57a57f8b0e8d59eeaac7da7127a0d2f48097030
marcocslima <marcocslima@gmail.com>
2023-05-28 20:30:00 +0000

    start cpp03

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcesar-d <mcesar-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 19:19:03 by mcesar-d          #+#    #+#             */
+/*   Updated: 2023/05/28 19:46:06 by mcesar-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap {
+	private:
+		std::string		_name;
+		unsigned int	_hitPoint;
+		unsigned int	_energyPoint;
+		unsigned int	_attackDamage;
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap(void);
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif
